2144c2e11e09a95292244fb12965c9de54cc1ea4
Ryoji Yoshioka <yoshioka.ryoji@gmail.com>
2018-06-21 20:41:27 +0900

    Add const of TokenType

--- token/token.go
+++ token/token.go
@@ -2,6 +2,32 @@ package token
 
 type TokenType string
 
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifiers + literals
+	IDENT = "IDENT" // add, foobar, x, y, ...
+	INT   = "INT"   // 1343456
+
+	// Operators
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
+
 type Token struct {
 	Type    TokenType
 	Literal string
