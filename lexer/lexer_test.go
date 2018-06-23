f878dcad9aa6335ac627a5fa68264945e28987db
Ryoji Yoshioka <yoshioka.ryoji@gmail.com>
2018-06-23 14:37:08 +0900

    Extend test case of lexer_test.go

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -8,21 +8,25 @@ import (
 
 func TestNextToken(t *testing.T) {
 	input := `let five = 5;
-	let ten = 10;
+let ten = 10;
 
-	let add = fn(x, y) {
-		x + y;
-	};
+let add = fn(x, y) {
+	x + y;
+};
 
-	let result = add(five, ten);
-	!-/*5;
-	5 < 10 > 5;
+let result = add(five, ten);
+!-/*5;
+5 < 10 > 5;
 
-	if (5 < 10) {
-		return true;
-	} else {
-		return false;
-	}`
+if (5 < 10) {
+	return true;
+} else {
+	return false;
+}
+
+10 == 10;
+10 != 9;
+`
 
 	tests := []struct {
 		expectedType    token.TokenType
@@ -93,6 +97,14 @@ func TestNextToken(t *testing.T) {
 		{token.FALSE, "false"},
 		{token.SEMICOLON, ";"},
 		{token.RBRACE, "}"},
+		{token.INT, "10"},
+		{token.EQ, "=="},
+		{token.INT, "10"},
+		{token.SEMICOLON, ";"},
+		{token.INT, "10"},
+		{token.NOT_EQ, "!="},
+		{token.INT, "9"},
+		{token.SEMICOLON, ";"},
 		{token.EOF, ""},
 	}
 
