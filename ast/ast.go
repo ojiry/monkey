3a913e6b2bee0baf5564537dd5684a0c7b43f14a
Ryoji Yoshioka <yoshioka.ryoji@gmail.com>
2018-06-26 19:21:30 +0900

    Implement ast

--- ast/ast.go
+++ ast/ast.go
@@ -1,14 +1,19 @@
 package ast
 
+import "github.com/ojiry/monkey/token"
+
+// The base Node interface
 type Node interface {
 	TokenLiteral() string
 }
 
+// All statement nodes implement this
 type Statement interface {
 	Node
 	statementNode()
 }
 
+// All expression nodes implement this
 type Expression interface {
 	Node
 	expressionNode()
@@ -25,3 +30,22 @@ func (p *Program) TokenLiteral() string {
 		return ""
 	}
 }
+
+// Statements
+type LetStatement struct {
+	Token token.Token // the token.LET token
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode()       {}
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+
+// Expressions
+type Identifier struct {
+	Token token.Token // the token.IDENT token
+	Value string
+}
+
+func (i *Identifier) statementNode()       {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
