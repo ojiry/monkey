4dc2331d41830fc7a730148179cd229f62d586f7
Ryoji Yoshioka <yoshioka.ryoji@gmail.com>
2018-07-12 21:11:54 +0900

    Implement evaluator

---
+++ evaluator/evaluator.go
@@ -0,0 +1,15 @@
+package evaluator
+
+import (
+	"github.com/ojiry/monkey/ast"
+	"github.com/ojiry/monkey/object"
+)
+
+func Eval(node ast.Node) object.Object {
+	switch node := node.(type) {
+	case *ast.IntegerLiteral:
+		return &object.Integer{Value: node.Value}
+	}
+
+	return nil
+}
